08f786cb281812f302f1d1413d549e84dfe70697
Osman Ocak <osmanocak02@yahoo.com>
2021-09-18 18:01:17 +0300

    creating test, before and after method

--- src/test/java/com/moneygaming/tests/test1.java
+++ src/test/java/com/moneygaming/tests/test1.java
@@ -1,6 +1,25 @@
 package com.moneygaming.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class test1 {
 
+    @BeforeMethod
+    public void setUp(){
+
+    }
+
+
+    @Test
+    public void test1(){
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+    }
 
 }
