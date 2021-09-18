3c673870371227d1537a1e53514969beb06d3bd3
Osman Ocak <osmanocak02@yahoo.com>
2021-09-18 18:08:40 +0300

    creating WebDriverFactory

---
+++ src/test/java/com/moneygaming/utilities/WebDriverFactory.java
@@ -0,0 +1,37 @@
+package com.moneygaming.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Locale;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType){
+
+        WebDriver driver = null;
+
+        switch (browserType.toLowerCase(Locale.ROOT)) {
+
+            case "chrome":
+
+                WebDriverManager.chromedriver().setup();
+
+                driver = new ChromeDriver();
+
+                break;
+
+            case "firefox":
+
+                WebDriverManager.firefoxdriver().setup();
+
+                driver = new FirefoxDriver();
+
+                break;
+        }
+
+        return driver;
+    }
+}
