c3efd0a1e9ab3d9f9187279a1bc8622de4bd028e
Osman Ocak <osmanocak02@yahoo.com>
2021-09-18 19:37:16 +0300

    ocakbranch commit

---
+++ src/test/java/com/moneygaming/tests/OcakTest.java
@@ -0,0 +1,11 @@
+package com.moneygaming.tests;
+
+import org.testng.annotations.Test;
+
+public class OcakTest {
+
+    @Test
+    public void testOcak(){
+        System.out.println("first");
+    }
+}
